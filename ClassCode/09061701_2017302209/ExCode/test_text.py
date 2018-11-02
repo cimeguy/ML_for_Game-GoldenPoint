60aa48e37babd35107c6eecbcf0b27f1f3663ced
Shawn-PC\Shawn904 <11491305@qq.com>
2018-11-02 22:02:33 +0800

    更新morse码

--- ClassCode/09061701_2017302209/ExCode/test_text.py
+++ ClassCode/09061701_2017302209/ExCode/test_text.py
@@ -3,7 +3,8 @@
 from textEx.text_hw import *
 
 
-if __name__ == '__main__':
+# 题目：词频统计
+def test_word():
     li = word_freq('text0.txt')
     print(li)
     li = word_freq('text1.txt')
@@ -14,3 +15,14 @@ if __name__ == '__main__':
     print(li)
     li = word_freq('sight word.txt')
     print(li)
+	
+
+# 题目：morse code
+def test_morse():
+    s = morse_code("i am morse 258")
+	# 正确输出：
+    # . .       . -   - -       - -   - - -   . - .   . . .   .       . . - - -   . . . . .   - - - . .
+    print(s)
+
+if __name__ == '__main__':
+    test_morse()
