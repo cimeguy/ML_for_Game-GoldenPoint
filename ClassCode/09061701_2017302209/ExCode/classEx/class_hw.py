4fe1ae07efc1edd5b0adb70d2cb56ef07e3ec092
Shawn <11491305@qq.com>
2019-01-16 20:57:35 +0800

    回退 'Pull Request !38 : 作业1-12'

---
+++ ClassCode/09061701_2017302209/ExCode/classEx/class_hw.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+
+# 题目八：计算图形面积及周长
+class Shape(object):
+    def __init__(self,name):
+        pass
+		
+    def cal_area(self):
+        pass
+		
+    def cal_perimeter(self):
+        pass
+		
+    def display(self):
+        pass
+		
+
+class Rectangle(Shape):
+    pass
+
+
+class Triangle(Shape):
+    pass
+
+
+class Circle(Shape):
+    pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ ClassCode/09061701_2017302209/ExCode/numEx/num_hw.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+
+# 题目一：二进制与十进制的相互转换
+def d2b(decimal_int):
+    pass
+
+
+def b2d(binary_string, flag = False):
+    pass
+
+
+# 题目二：类似找零钱的操作
+money=[50,20,10,5,1,0.5,0.1]  # 纸币面额
+item_dict = {'item01' : 2.3,  'item02' : 35.8, 'item03' : 16.3, 'item04' : 12,
+        'item05' : 13.6, 'item06' : 29,   'item07' : 17.4, 'item08' : 63.9,
+        'item09' : 56.7, 'item10' : 23.8}  # 商品名称与价格
+
+
+def list_goods():
+    pass
+
+
+def get_changes(items, pay):
+    pass
+		
+
+# 题目三：两地之间距离计算
+# 计算两点p1, p2之间的距离
+# p1：二元组，（纬度、经度）
+# p2: 二元组，（纬度、经度）
+def sphere_distance(p1, p2):
+    pass
+		
+
+# 题目四：计算Fibonacci 序列的值
+# Fibonacci是1，1, 2，3，5, 8，13.....
+# n1 = 1, n2 =2, n3 = n1+n2, n4 = n3+n2
+def fibonacci_recursion(number):
+    pass
+
+
+def fibonacci_loop(number):
+    pass
+
+
+if __name__ == '__main__':
+    pass
